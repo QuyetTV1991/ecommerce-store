ad6f6b129ccabe7a11c18309e203401e053cd01e
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-02-24 03:14:10 +0000

    Pending changes exported from your codespace

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Phudu } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const phudu = Phudu({
+  subsets: ['latin', 'vietnamese'],
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-phudu',
+  style: ['normal']
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,6 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Common favicons for all devices */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/favicon-192x192.png" />
+        {/* iOS icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        {/* Other favicons for specific devices or resolutions */}
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon/favicon-512x512.png" />
+        {/* Add more favicon sizes as needed */}
+        
+        {/* Set a default favicon */}
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
