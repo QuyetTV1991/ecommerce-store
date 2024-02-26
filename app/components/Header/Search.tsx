8b9ff960443ac9909dce6b9d529afae084dd237e
QuyetTV1991 <quyettv.bk@gmail.com>
2024-02-26 21:17:09 +0800

    border search box

--- app/components/Header/Search.tsx
+++ app/components/Header/Search.tsx
@@ -3,8 +3,11 @@ import { Input } from "../ui/input";
 
 const Search = () => {
   return (
-    <div className="flex justify-between items-center gap-4">
-      <Input className="rounded-[25px]" />
+    <div className="flex justify-between items-center gap-4 px-10 mx-8">
+      <Input
+        className="rounded-[10px] text-gray-400 border border-blue-600 border-solid"
+        placeholder="Search"
+      />
       <div>Filter</div>
     </div>
   );
